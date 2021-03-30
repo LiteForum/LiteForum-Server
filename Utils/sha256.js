c9d8fe257af8f5fe5b3e172d9a77987eabd7d46f
Kurokitu <foxiti.a@gmail.com>
2021-03-30 17:54:26 +0800

    gitignore working

--- Utils/sha256.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-let sha256 = (data) => {
-    let sha256 = crypto.createHash('sha256');
-    return sha256.update(data).digest('hex');
-}
-
-module.exports = sha256
